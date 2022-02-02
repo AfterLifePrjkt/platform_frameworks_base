18a2ad8b668ae04108ab96f55a285c9f26674f11
cjybyjk <cjybyjk@zjnu.edu.cn>
2022-02-02 23:52:39 +0800

    base: Add support for per-app volume [2/3]
    [ghostrider-reborn]
    - Use app volume icon by amartya (@ yukiscape)
    
    Change-Id: I674cda1d7463b3603ccaac673178aeead4093619
    Co-authored-by: yukiscape <garikapatiamartya@gmail.com>
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Mr- 0 <oldcrocs21@gmail.com>
    Signed-off-by: X z4 <pajapandia21@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6291,6 +6291,12 @@ public final class Settings {
         }
 
         /**
+         * Show app volume rows in volume panel
+         * @hide
+         */
+        public static final String SHOW_APP_VOLUME = "show_app_volume";
+
+        /**
          * When to use Wi-Fi calling
          *
          * @see android.telephony.TelephonyManager.WifiCallingChoices
@@ -17908,6 +17914,24 @@ public final class Settings {
         @SdkConstant(SdkConstant.SdkConstantType.ACTIVITY_INTENT_ACTION)
         public static final String ACTION_VOLUME =
                 "android.settings.panel.action.VOLUME";
+
+        /**
+         * @hide
+         */
+        public static final String ACTION_APP_VOLUME =
+                "android.settings.panel.action.APP_VOLUME";
+
+        /**
+         * Activity Action: Show a settings dialog containing controls for Mobile Data.
+         * <p>
+         * Input: Nothing.
+         * <p>
+         * Output: Nothing.
+         * @hide
+         */
+        @SdkConstant(SdkConstant.SdkConstantType.ACTIVITY_INTENT_ACTION)
+        public static final String ACTION_MOBILE_DATA =
+                "android.settings.panel.action.MOBILE_DATA";
 
         /**
          * Activity Action: Show a settings dialog containing controls for Bluetooth.
