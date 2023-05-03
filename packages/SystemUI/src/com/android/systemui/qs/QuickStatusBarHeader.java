27c28d314541e66089ebd86067a2a10cd8d34ae3
Mr_ZerO <rbesar210311@gmail.com>
2023-05-03 14:48:14 +0000

    SystemUI: Open Weather settings on long click
    Signed-off-by: Mr_ZerO <rbesar210311@gmail.com>
    Signed-off-by: Mr- 0 <oldcrocs21@gmail.com>
    Signed-off-by: X z4 <pajapandia21@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -150,6 +150,7 @@ public class QuickStatusBarHeader extends FrameLayout implements
         mClockDateView = findViewById(R.id.date_clock);
         mDateView.setOnClickListener(this);
         mQsWeatherHeaderView = findViewById(R.id.weather_view_header);
+        mQsWeatherHeaderView.setOnLongClickListener(this);
         mClockDateView.setVisibility(View.GONE);
         mSecurityHeaderView = findViewById(R.id.header_text_container);
         mClockIconsSeparator = findViewById(R.id.separator);
@@ -260,6 +261,13 @@ public class QuickStatusBarHeader extends FrameLayout implements
             nIntent.setClassName("com.android.settings",
                     "com.android.settings.Settings$DateTimeSettingsActivity");
             mActivityStarter.startActivity(nIntent, true /* dismissShade */);
+            mVibrator.vibrate(VibrationEffect.createOneShot(50, VibrationEffect.DEFAULT_AMPLITUDE));
+            return true;
+        } else if (v == mQsWeatherHeaderView) {
+            Intent wIntent = new Intent(Intent.ACTION_MAIN);
+            wIntent.setClassName("org.omnirom.omnijaws",
+                    "org.omnirom.omnijaws.SettingsActivity");
+            mActivityStarter.startActivity(wIntent, true /* dismissShade */);
             mVibrator.vibrate(VibrationEffect.createOneShot(50, VibrationEffect.DEFAULT_AMPLITUDE));
             return true;
         }
