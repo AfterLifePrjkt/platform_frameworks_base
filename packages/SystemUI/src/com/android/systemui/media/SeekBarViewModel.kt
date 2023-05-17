6ae0d01679e4bb09518ac9d29fb03baf6af06a15
Mr_ZerO <rbesar210311@gmail.com>
2023-05-17 02:04:08 +0000

    Revert "Pause squiggle animation when seeking"
    This reverts commit 167a64be472dbc76dd5144637cb34d5731097653.

--- packages/SystemUI/src/com/android/systemui/media/SeekBarViewModel.kt
+++ packages/SystemUI/src/com/android/systemui/media/SeekBarViewModel.kt
@@ -74,7 +74,7 @@ private fun PlaybackState.computePosition(duration: Long): Long {
 class SeekBarViewModel @Inject constructor(
     @Background private val bgExecutor: RepeatableExecutor
 ) {
-    private var _data = Progress(false, false, false, false, null, 0)
+    private var _data = Progress(false, false, false, null, 0)
         set(value) {
             field = value
             _progress.postValue(value)
@@ -127,7 +127,6 @@ class SeekBarViewModel @Inject constructor(
             if (field != value) {
                 field = value
                 checkIfPollingNeeded()
-                _data = _data.copy(scrubbing = value)
             }
         }
 
@@ -199,7 +198,7 @@ class SeekBarViewModel @Inject constructor(
         val enabled = if (playbackState == null ||
                 playbackState?.getState() == PlaybackState.STATE_NONE ||
                 (duration <= 0)) false else true
-        _data = Progress(enabled, seekAvailable, playing, scrubbing, position, duration)
+        data = Progress(enabled, seekAvailable, playing, position, duration)
         checkIfPollingNeeded()
     }
 
@@ -417,7 +416,6 @@ class SeekBarViewModel @Inject constructor(
         val enabled: Boolean,
         val seekAvailable: Boolean,
         val playing: Boolean,
-        val scrubbing: Boolean,
         val elapsedTime: Int?,
         val duration: Int
     )
