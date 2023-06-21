3c867b3976462cef8995b1bc13e75a87b78f91b5
X z4 <pajapandia21@gmail.com>
2023-06-21 13:54:27 +0000

    Revert "SystemUI: Allow choosing volume panel position [1/2]"
    This reverts commit a2e6433ef64a65c1e7a6e74c006d966e28aa8587.

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -85,7 +85,6 @@ public class SystemSettings {
         Settings.System.ALARM_ALERT,
         Settings.System.NOTIFICATION_LIGHT_PULSE,
         Settings.System.FULLSCREEN_GESTURES,
-        Settings.System.VOLUME_PANEL_ON_LEFT,
         Settings.System.LOCKSCREEN_SMALL_CLOCK,
     };
 }
